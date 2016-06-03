a0f543f764c871e6de47a6833d63424abac920d4
Matej Szendi <matej.szendi@bcom.cz>
2016-06-03 08:33:33 +0200

    * Update dependencies * Compatibility with ZF2 and ZF3

--- src/LemoTracy/Options/ModuleOptionsFactory.php
+++ src/LemoTracy/Options/ModuleOptionsFactory.php
@@ -2,7 +2,8 @@
 
 namespace LemoTracy\Options;
 
-use Zend\ServiceManager\FactoryInterface;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 class ModuleOptionsFactory implements FactoryInterface
@@ -13,13 +14,28 @@ class ModuleOptionsFactory implements FactoryInterface
     const CONFIG_KEY = 'lemo_tracy';
 
     /**
-     * @param  ServiceLocatorInterface $services
+     * Create an object
+     *
+     * @param  ContainerInterface $container
+     * @param  string             $requestedName
+     * @param  null|array         $options
      * @return ModuleOptions
      */
-    public function createService(ServiceLocatorInterface $services)
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        $config = $services->get('Config');
+        $config = $container->get('config');
 
         return new ModuleOptions(empty($config[$this::CONFIG_KEY]) ? [] : $config[$this::CONFIG_KEY]);
+    }
+
+    /**
+     * Create an object (v2)
+     *
+     * @param  ServiceLocatorInterface $container
+     * @return ModuleOptions
+     */
+    public function createService(ServiceLocatorInterface $container)
+    {
+        return $this($container, ModuleOptions::class);
     }
 }
